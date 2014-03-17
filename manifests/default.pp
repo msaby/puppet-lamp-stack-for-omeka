55a89f7948c4eaeeb62c771f91aa5f0a284180b6
Michael <mkappel1@swarthmore.edu>
2014-03-17 16:15:05 -0400

    Added bash script to auto install Omeka

--- manifests/default.pp
+++ manifests/default.pp
@@ -11,3 +11,11 @@ include php::pear
 include php::pecl
 include mysql
 
+file { '/opt/omeka-autoinstall.sh':
+	name	=> '/opt/omeka-autoinstall.sh',
+	ensure	=> present,
+	source	=> '/vagrant/files/omeka-autoinstall.sh',
+    owner  => 'www-data',
+    group  => 'vagrant',
+    mode   => '0700',
+}
