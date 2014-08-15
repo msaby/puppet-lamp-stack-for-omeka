0227d0bef0210ea6a2910aec383bb13429ea1124
Michael <kappeler.michael@gmail.com>
2014-08-15 10:22:47 -0400

    Update to grab Omeka 2.2.2

--- modules/omeka/manifests/init.pp
+++ modules/omeka/manifests/init.pp
@@ -15,28 +15,28 @@ class omeka {
     command => 'mysql -u root -pd3v0p5 --execute="GRANT ALL PRIVILEGES ON omeka.* TO \'omeka\'@\'localhost\' IDENTIFIED BY \'omeka\'"',
   }
 
-  # Download Omeka: http://omeka.org/files/omeka-2.1.4.zip
+  # Download Omeka: http://omeka.org/files/omeka-2.2.2.zip
 
   exec { 'download-omeka':
-    command => 'wget http://omeka.org/files/omeka-2.1.4.zip',
+    command => 'wget http://omeka.org/files/omeka-2.2.2.zip',
     cwd     => '/vagrant/',
-    creates => '/vagrant/omeka-2.1.4.zip'
+    creates => '/vagrant/omeka-2.2.2.zip'
   }
 
   exec { 'unzip-omeka':
     cwd     => '/vagrant/',
-    command => 'unzip omeka-2.1.4.zip',
+    command => 'unzip omeka-2.2.2.zip',
     require => Exec['download-omeka'],
-    creates => '/vagrant/omeka-2.1.4',
+    creates => '/vagrant/omeka-2.2.2',
   }
 
   exec { 'move-omeka':
     require => Exec['unzip-omeka'],
-    command => 'sudo mv /vagrant/omeka-2.1.4/* /vagrant/webroot/ | sudo mv /vagrant/omeka-2.1.4/.htaccess /vagrant/webroot/',
+    command => 'sudo mv /vagrant/omeka-2.2.2/* /vagrant/webroot/ | sudo mv /vagrant/omeka-2.2.2/.htaccess /vagrant/webroot/',
     }
   exec {'cleanup-omeka':
     require => Exec['move-omeka'],
-    command => 'sudo rm /vagrant/omeka-2.1.4.zip | sudo rm -R /vagrant/omeka-2.1.4 | sudo rm /vagrant/webroot/db.ini',
+    command => 'sudo rm /vagrant/omeka-2.2.2.zip | sudo rm -R /vagrant/omeka-2.2.2 | sudo rm /vagrant/webroot/db.ini',
     }
 
   # Copy db.ini
